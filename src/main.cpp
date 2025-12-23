2b2522f57016a4bb10a1db6c08399f0db2ddc4e7
Nathan Barguss <Nathan.Barguss@sony.com>
2025-12-23 19:14:22 +0000

    Refactor main sketch to use display interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,32 @@
 #include <Arduino.h>
 #include <avr/io.h>
 #include <util/delay.h>
-#include <LiquidCrystal.h>
-
 #include <stdio.h>
 
-// vim: ts=4 ai
-#define LED_PIN 10		// This is the pin the backlight is controlled by, it must be a PWM pin
-#define STARTUP_BRIGHTNESS 2  // What backlight brightness to start up with (8-bit value)
-#define BAUDRATE 57600 // What baudrate to use for the serial port
-#define LCDW 20  // LCD column length
-#define LCDH 4   // LCD number of rows
+#include <DisplayConfig.h>
 
-#define HD44780 0
-#define OLED 1
-
-#ifndef DISPLAY_BACKEND
-#define DISPLAY_BACKEND HD44780
-#endif
+#include "display/display_factory.h"
 
 #if DISPLAY_BACKEND != HD44780
 #error "Only the HD44780 backend is implemented; update the firmware before selecting another backend."
 #endif
 
-// initialize the library with the numbers of the interface pins
-/*	Note, while all the ardu documentation and schematics show 4-bit operation. 
-	It seems the library actually supports both 8-bit and 4-bit mode. It attempts
-	8-bit, then falls back to 4-bit */
-LiquidCrystal lcd(12, 11, 2, 3, 4, 5, 6, 7, 8, 9);
-
-
 byte cmd; //will hold our sent command
 
-
-void set_backlight(int value) {
-	// We can control the backlight via PWM here, range from 0 to 255 values
-	// 0 is "off", 255 is "on" (max brightness). 
-	analogWrite(LED_PIN, value);
-}
+static IDisplay &display = getDisplay();
 
 void setup() {
-	pinMode(LED_PIN, OUTPUT);			// set pin to output
-	// We first set the backlight brightness
-	set_backlight(STARTUP_BRIGHTNESS);
-
 	// set up the LCD's number of columns and rows:
-	lcd.begin(LCDW, LCDH);
+	display.begin(LCDW, LCDH);
+	display.setBacklight(STARTUP_BRIGHTNESS);
 	// set up serial
 	Serial.begin(BAUDRATE);
-	lcd.display();
-	lcd.clear();
+	display.display();
+	display.clear();
 	char welcome[LCDW];
 	sprintf(welcome, "%dx%d Ready", LCDW, LCDH);
-	lcd.write(welcome);
-	lcd.home();
+	display.write(welcome);
+	display.home();
 
 }
 
@@ -93,15 +66,15 @@ void loop() {
 	cmd = serial_read();
 	switch(cmd) {
 			case 0xFE:
-				lcd.command(serial_read());
+				display.command(serial_read());
 				break;
 			case 0xFD:
 				// backlight control
-				set_backlight(serial_read());
+				display.setBacklight(serial_read());
 				break;
 			default:
 				// By default we write to the LCD
-				lcd.write(cmd);
+				display.write(cmd);
 				break;
 	}
 }
